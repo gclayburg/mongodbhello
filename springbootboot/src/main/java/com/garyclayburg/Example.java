62acea8ebe7e32644596eabdae669786a8c2c076
Gary Clayburg <gclaybur@comcast.net>
2014-03-26 11:09:57 -0600

    reorganized maven dependencies to use spring-boot parent

---
+++ springbootboot/src/main/java/com/garyclayburg/Example.java
@@ -0,0 +1,50 @@
+/*
+ * VisualSync - a tool to visualize user data synchronization
+ * Copyright (c) 2014 Gary Clayburg
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.garyclayburg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 3/25/14
+ * Time: 2:40 PM
+ *
+ * @author Gary Clayburg
+ */
+@Controller
+@EnableAutoConfiguration
+public class Example {
+    private static final Logger log = LoggerFactory.getLogger(Example.class);
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "hello!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class,args);
+    }
+}
