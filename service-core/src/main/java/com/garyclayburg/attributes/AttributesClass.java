dc7e82d0a9ef0490fd3491e3f0a175785c289c61
Gary Clayburg <gclaybur@comcast.net>
2014-04-04 20:42:08 -0600

    converted AttributesClass.groovy to AttributesClass.java

---
+++ service-core/src/main/java/com/garyclayburg/attributes/AttributesClass.java
@@ -0,0 +1,38 @@
+/*
+ * VisualSync - a tool to visualize user data synchronization
+ * Copyright (c) 2014 Gary Clayburg
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.garyclayburg.attributes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 4/3/14
+ * Time: 11:09 AM
+ *
+ * @author Gary Clayburg
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.TYPE)
+public @interface AttributesClass {
+
+}
