cfc85ad29b6f8bed8a0ddf9de583ba1f1c191929
Gary Clayburg <gclaybur@comcast.net>
2014-05-09 08:50:45 -0600

    initial wiring of vconsole to users

--- service-core/src/main/java/com/garyclayburg/vconsole/VConsole.java
+++ service-core/src/main/java/com/garyclayburg/vconsole/VConsole.java
@@ -18,12 +18,20 @@
 
 package com.garyclayburg.vconsole;
 
+import com.garyclayburg.persistence.domain.User;
+import com.garyclayburg.persistence.repository.AutoUserRepo;
+import com.vaadin.data.Container;
+import com.vaadin.data.Item;
+import com.vaadin.data.util.BeanContainer;
+import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.VaadinUI;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,8 +46,10 @@ public class VConsole extends UI {
     @SuppressWarnings("UnusedDeclaration")
     private static final Logger log = LoggerFactory.getLogger(VConsole.class);
 
+    @Autowired
+    AutoUserRepo autoUserRepo;
+
     protected void init(VaadinRequest vaadinRequest) {
-/*
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
@@ -51,8 +61,45 @@ public class VConsole extends UI {
             }
         });
         layout.addComponent(button);
-*/
+        List<User> allUsers = autoUserRepo.findAll();
+        for (User allUser : allUsers) {
+            layout.addComponent(new Label(allUser.getFirstname()));
+        }
 
-        setContent(new Label("Hello vaadin spring"));
+        Table staticTable = new Table();
+        staticTable.setSizeFull();
+        staticTable.setSelectable(true);
+        staticTable.setMultiSelect(false);
+        staticTable.setImmediate(true);
+
+        createStatic(layout,staticTable);
+
+        BeanContainer<String, User> userBeanContainer = new BeanContainer<String, User>(User.class);
+        userBeanContainer.setBeanIdProperty("firstname");
+
+        for (User user : allUsers) {
+            userBeanContainer.addBean(user);
+        }
+        Table userTable = new Table();
+        userTable.setSizeFull();
+        userTable.setSelectable(true);
+        userTable.setMultiSelect(false);
+        userTable.setImmediate(true);
+        userTable.setContainerDataSource(userBeanContainer);
+        layout.addComponent(userTable);
+    }
+
+    private void createStatic(VerticalLayout layout,Table staticTable) {
+        Container container = new IndexedContainer();
+        container.addContainerProperty("first",String.class,"unknown");
+        container.addContainerProperty("last",String.class,"na");
+        Item r1  = container.addItem("row1");
+        r1.getItemProperty("first").setValue("Frank");
+        r1.getItemProperty("last").setValue("Clayburg");
+        Item r2  = container.addItem("row2");
+        r2.getItemProperty("first").setValue("Claribel");
+        r2.getItemProperty("last").setValue("Bell");
+        staticTable.setContainerDataSource(container);
+        layout.addComponent(staticTable);
     }
 }
