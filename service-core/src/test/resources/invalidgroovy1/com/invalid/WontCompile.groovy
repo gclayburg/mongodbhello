4b86b708e949656c99b9d9f3b340442d0ea004f4
Gary Clayburg <gclaybur@comcast.net>
2014-06-25 12:32:03 -0600

    cleaned up superfluous messages in test log

--- service-core/src/test/resources/invalidgroovy1/com/invalid/WontCompile.groovy
+++ service-core/src/test/resources/invalidgroovy1/com/invalid/WontCompile.groovy
@@ -16,4 +16,4 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-this is a sentence it should not compile
+this is not the stacktrace you are looking for.
