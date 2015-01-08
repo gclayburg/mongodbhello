33499b4200f229e76a4eb6e506acbbc88a0db621
Gary Clayburg <gclaybur@comcast.net>
2015-01-08 10:40:41 -0700

    chosen deployed server endpoint is now exported to properties file for downstream build testing

--- docker/visualsync/deploycoreos.sh
+++ docker/visualsync/deploycoreos.sh
@@ -24,5 +24,11 @@ fi
 
 myurl=$(echo $url | jq -r '.url')
 echo "url is: $myurl"
+ENDPOINT="$(echo $url | jq -r '.host'):$(echo $url | jq -r '.port')"
+echo "endpoint is: $ENDPOINT"
+#create properties file on descriptor #3,
+# stdout contains log output of all commands in this script
+# stderr contains normal errors
+>&3 echo "ENDPOINT=$ENDPOINT"
 isTomcatRunning.sh $myurl
 
