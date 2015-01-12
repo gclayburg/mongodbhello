964213d47e9bdcf28b8b2605f76773ef69888d22
Gary Clayburg <gclaybur@comcast.net>
2015-01-12 10:52:45 -0700

    generic version of deploy

--- docker/visualsync/deploycoreos.sh
+++ docker/visualsync/deploycoreos.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 date
+INSTANCE=${INSTANCE:-9}
 fleetctl --version
 fleetctl -tunnel mink -strict-host-key-checking=false list-machines
-fleetctl -tunnel mink -strict-host-key-checking=false stop console@9.service
-fleetctl -tunnel mink -strict-host-key-checking=false stop console-discovery@9.service
-fleetctl -tunnel mink -strict-host-key-checking=false stop empty-mongodb@9.service
-fleetctl -tunnel mink -strict-host-key-checking=false unload console-discovery@9.service
+fleetctl -tunnel mink -strict-host-key-checking=false stop console@${INSTANCE}.service
+fleetctl -tunnel mink -strict-host-key-checking=false stop console-discovery@${INSTANCE}.service
+fleetctl -tunnel mink -strict-host-key-checking=false stop empty-mongodb@${INSTANCE}.service
+fleetctl -tunnel mink -strict-host-key-checking=false unload console-discovery@${INSTANCE}.service
 fleetctl -tunnel mink -strict-host-key-checking=false list-units
 echo "console should be dead now"
 date
 #sleep 30
-fleetctl -tunnel mink -strict-host-key-checking=false start console@9.service
-fleetctl -tunnel mink -strict-host-key-checking=false start console-discovery@9.service
-fleetctl -tunnel mink -strict-host-key-checking=false start empty-mongodb@9.service
+fleetctl -tunnel mink -strict-host-key-checking=false start console@${INSTANCE}.service
+fleetctl -tunnel mink -strict-host-key-checking=false start console-discovery@${INSTANCE}.service
+fleetctl -tunnel mink -strict-host-key-checking=false start empty-mongodb@${INSTANCE}.service
 fleetctl -tunnel mink -strict-host-key-checking=false list-units
-if url=$(etcdctl -C=http://mink:4001 get /services/website/console@9); then
+date
+   echo "checking key: /services/website/console@${INSTANCE}..."
+if url=$(etcdctl -C=http://mink:4001 get /services/website/console@${INSTANCE}); then
   echo ok
 else
-   echo "wait for it..."
-   url=$(etcdctl -C=http://mink:4001 watch /services/website/console@9)
+   echo "waiting for key: /services/website/console@${INSTANCE}..."
+   url=$(etcdctl -C=http://mink:4001 watch /services/website/console@${INSTANCE})
 fi
-
+date
 myurl=$(echo $url | jq -r '.url')
 echo "url is: $myurl"
 ENDPOINT="$(echo $url | jq -r '.host'):$(echo $url | jq -r '.port')"
