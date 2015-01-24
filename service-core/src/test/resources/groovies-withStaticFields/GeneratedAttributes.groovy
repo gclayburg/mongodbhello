b486e530e40f05b7fc43311d98cb75e183b10b9b
Gary Clayburg <gclaybur@comcast.net>
2015-01-24 12:04:04 -0700

    rest-only module

---
+++ service-core/src/test/resources/groovies-withStaticFields/GeneratedAttributes.groovy
@@ -0,0 +1,53 @@
+//package com.acme
+
+import com.garyclayburg.attributes.AttributesClass
+import com.garyclayburg.attributes.TargetAttribute
+import com.garyclayburg.persistence.domain.User
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 3/27/14
+ * Time: 12:42 PM
+ *
+ * @author Gary Clayburg
+ */
+@AttributesClass
+public class GeneratedAttributes {
+
+    @TargetAttribute(target = Targets.LDAP,attributeName = "cn")
+    static String cn(User user){
+        return user.firstname +" "+ user.lastname
+    }
+
+    @TargetAttribute(target = Targets.BCT, attributeName = "cn")
+    static adcn(User user){
+        return cn(user)
+    }
+
+    //default attributeName is the name of the method - "objectclass" in this case
+    @TargetAttribute(target = Targets.LDAP)
+    static String objectclass(User user){
+        return "top person organizationalperson inetorgperson"
+    }
+
+    @TargetAttribute(target = Targets.BCT,attributeName = "displayName")
+    static String buildDisplayName(User user){
+        def var
+        if (user.lastname == null || user.lastname.equals("")){
+            var = user.firstname
+        } else{
+            var = user.lastname + ", "+ user.firstname
+        }
+        return var;
+    }
+
+    @TargetAttribute
+    static String cityState(User user){
+        return "Kansas City, Kansas "
+    }
+
+    @TargetAttribute
+    static String streetAddress(User user){
+        return "114 Lincoln Way"
+    }
+}
---
+++ service-core/src/test/resources/groovies-withStaticFields/o2cAttributes.groovy
@@ -0,0 +1,38 @@
+//package com.acme
+
+import com.garyclayburg.attributes.AttributesClass
+import com.garyclayburg.attributes.TargetAttribute
+import com.garyclayburg.persistence.domain.User
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 3/27/14
+ * Time: 12:42 PM
+ *
+ * @author Gary Clayburg
+ */
+
+class TargetIn {
+    public static final BCT="Active Directory BCT Domain"
+}
+
+@AttributesClass
+class o2cAttributes {
+
+    @TargetAttribute(target = TargetIn.BCT,attributeName = "dept")
+    static String department(User user){
+        return "dept 71" + Targets.BCT
+    }
+
+    @TargetAttribute(target = TargetIn.BCT)
+    static String address1(User user){
+        return GeneratedAttributes.streetAddress(user)
+    }
+
+    @TargetAttribute(target = Targets.BCT)
+    static String userid(User user){
+        return user.id
+    }
+
+}
+
