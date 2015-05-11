69e42d2a985b43870d61ef69cea87625a99d1aa2
Gary Clayburg <gclaybur@comcast.net>
2015-05-11 10:44:28 -0600

    now using explicit endpoint for fleetctl deployment

--- docker/visualsync/startcoreos.sh
+++ docker/visualsync/startcoreos.sh
@@ -3,8 +3,8 @@
 date
 INSTANCE=${INSTANCE:-${1:-9}} # order of preference: env.INSTANCE, $1, 9
 echo "starting instance $INSTANCE"
-fleetctl -tunnel mink -strict-host-key-checking=false start console@${INSTANCE}.service
-fleetctl -tunnel mink -strict-host-key-checking=false start console-discovery@${INSTANCE}.service
-fleetctl -tunnel mink -strict-host-key-checking=false start empty-mongodb@${INSTANCE}.service
-fleetctl -tunnel mink -strict-host-key-checking=false list-units
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start console@${INSTANCE}.service
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start console-discovery@${INSTANCE}.service
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start empty-mongodb@${INSTANCE}.service
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-units
 
