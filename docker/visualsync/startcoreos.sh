5c058e56b6c9b5890da69f80713fc262de30ae96
Gary Clayburg <gclaybur@comcast.net>
2015-05-11 10:53:07 -0600

    troubleshooting coreos deployment

--- docker/visualsync/startcoreos.sh
+++ docker/visualsync/startcoreos.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+date_echo(){
+    datestamp=$(date +%F_%T)
+    echo "${datestamp} $*"
+}
+
+do_shell(){
+# Execute command in shell, while logging complete command to stdout
+    echo "$(date +%F_%T) --> $*"
+    eval "$*"
+    STATUS=$?
+    return $STATUS
+}
+
 date
 INSTANCE=${INSTANCE:-${1:-9}} # order of preference: env.INSTANCE, $1, 9
-echo "starting instance $INSTANCE"
+date_echo "starting console instance $INSTANCE"
 fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start console@${INSTANCE}.service
+date_echo "starting console-discovery instance $INSTANCE"
 fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start console-discovery@${INSTANCE}.service
+date_echo "starting empty-mongodb instance $INSTANCE"
 fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start empty-mongodb@${INSTANCE}.service
+date_echo "list-units"
 fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-units
 
