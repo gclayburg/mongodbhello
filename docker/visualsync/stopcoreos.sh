99208ce365ce94c715fba4eb27cbf8c583bc8242
Gary Clayburg <gclaybur@comcast.net>
2015-05-13 09:24:43 -0600

    hardcoded coreos endpoint IP addresses

--- docker/visualsync/stopcoreos.sh
+++ docker/visualsync/stopcoreos.sh
@@ -4,13 +4,13 @@ date
 INSTANCE=${INSTANCE:-${1:-9}} # order of preference: env.INSTANCE, $1, 9
 echo "stopping console@${INSTANCE}"
 fleetctl --version
-fleetctl -tunnel mink -strict-host-key-checking=false list-machines
-fleetctl -tunnel mink -strict-host-key-checking=false stop console@${INSTANCE}.service
-fleetctl -tunnel mink -strict-host-key-checking=false stop console-discovery@${INSTANCE}.service
-fleetctl -tunnel mink -strict-host-key-checking=false stop empty-mongodb@${INSTANCE}.service
-fleetctl -tunnel mink -strict-host-key-checking=false unload console-discovery@${INSTANCE}.service
-#fleetctl -tunnel mink -strict-host-key-checking=false unload console@${INSTANCE}.service
-#fleetctl -tunnel mink -strict-host-key-checking=false unload empty-mongodb@${INSTANCE}.service
-fleetctl -tunnel mink -strict-host-key-checking=false list-units
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-machines
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop console@${INSTANCE}.service
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop console-discovery@${INSTANCE}.service
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop empty-mongodb@${INSTANCE}.service
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload console-discovery@${INSTANCE}.service
+#fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload console@${INSTANCE}.service
+#fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload empty-mongodb@${INSTANCE}.service
+fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-units
 echo "console should be dead now"
 date
