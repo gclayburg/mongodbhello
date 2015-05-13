025ffec1a8709310f735cb983bbc0c297f20c5bf
Gary Clayburg <gclaybur@comcast.net>
2015-05-13 11:16:51 -0600

    standardizing messages

--- docker/visualsync/startcoreos.sh
+++ docker/visualsync/startcoreos.sh
@@ -18,12 +18,12 @@ do_shell_fail(){
 
 date
 INSTANCE=${INSTANCE:-${1:-9}} # order of preference: env.INSTANCE, $1, 9
-date_echo "starting console instance $INSTANCE from host $(uname -n)"
+date_echo "starting console@${INSTANCE} from host $(uname -n)"
 do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false submit console@.service
 do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start console@${INSTANCE}.service
-date_echo "starting console-discovery instance $INSTANCE"
+date_echo "starting console-discovery@${INSTANCE}"
 do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start console-discovery@${INSTANCE}.service
-date_echo "starting empty-mongodb instance $INSTANCE"
+date_echo "starting empty-mongodb@${INSTANCE}"
 do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false submit ../mongodb/empty-mongodb@.service
 do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start empty-mongodb@${INSTANCE}.service
 date_echo "list-units"
