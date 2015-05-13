38709a8dff88a68b2b0f4dcbbdc8cd2a9ae3cec6
Gary Clayburg <gclaybur@comcast.net>
2015-05-13 09:45:54 -0600

    extra debugging

--- docker/visualsync/stopcoreos.sh
+++ docker/visualsync/stopcoreos.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+date_echo(){
+    datestamp=$(date +%F_%T)
+    echo "${datestamp} $*"
+}
+
+do_shell(){
+# Execute command in shell, while logging complete command to stdout
+    echo "$(date +%F_%T) --> $*"
+    eval "$*"
+    STATUS=$?
+    return $STATUS
+}
+
 date
 INSTANCE=${INSTANCE:-${1:-9}} # order of preference: env.INSTANCE, $1, 9
-echo "stopping console@${INSTANCE}"
-fleetctl --version
-fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-machines
-fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop console@${INSTANCE}.service
-fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop console-discovery@${INSTANCE}.service
-fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop empty-mongodb@${INSTANCE}.service
-fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload console-discovery@${INSTANCE}.service
+date_echo "stopping console@${INSTANCE}"
+do_shell fleetctl --version
+do_shell fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-machines
+do_shell fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop console@${INSTANCE}.service
+do_shell fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop console-discovery@${INSTANCE}.service
+do_shell fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop empty-mongodb@${INSTANCE}.service
+do_shell fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload console-discovery@${INSTANCE}.service
 #fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload console@${INSTANCE}.service
 #fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload empty-mongodb@${INSTANCE}.service
-fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-units
-echo "console should be dead now"
-date
+do_shell fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-units
+date_echo "console should be dead now"
+
