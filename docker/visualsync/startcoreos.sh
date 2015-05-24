04beee16fcd38a42462fb543634fe6d02605af82
Gary Clayburg <gclaybur@comcast.net>
2015-05-24 17:00:24 -0600

    made coreos deployment more general

--- docker/visualsync/startcoreos.sh
+++ docker/visualsync/startcoreos.sh
@@ -5,13 +5,13 @@ LOG_CONTEXT="$0 -"
 
 INSTANCE=${INSTANCE:-${1:-9}} # order of preference: env.INSTANCE, $1, 9
 date_echo "starting console@${INSTANCE} from host $(uname -n)"
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false submit console@.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start console@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false submit console@.service
+do_shell_fail fleetctl -strict-host-key-checking=false start console@${INSTANCE}.service
 date_echo "starting console-discovery@${INSTANCE}"
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start console-discovery@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false start console-discovery@${INSTANCE}.service
 date_echo "starting empty-mongodb@${INSTANCE}"
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false submit ../mongodb/empty-mongodb@.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false start empty-mongodb@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false submit ../mongodb/empty-mongodb@.service
+do_shell_fail fleetctl -strict-host-key-checking=false start empty-mongodb@${INSTANCE}.service
 date_echo "list-units"
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-units
+do_shell_fail fleetctl -strict-host-key-checking=false list-units
 date_echo "console and mongodb should be starting now"
--- docker/visualsync/stopcoreos.sh
+++ docker/visualsync/stopcoreos.sh
@@ -7,20 +7,20 @@ LOG_CONTEXT="$0 -"
 INSTANCE=${INSTANCE:-${1:-9}} # order of preference: env.INSTANCE, $1, 9
 date_echo "stopping console@${INSTANCE} from host $(uname -n)"
 do_shell_fail fleetctl --version
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-machines
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop console@${INSTANCE}.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop console-discovery@${INSTANCE}.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false stop empty-mongodb@${INSTANCE}.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload console-discovery@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false list-machines
+do_shell_fail fleetctl -strict-host-key-checking=false stop console@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false stop console-discovery@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false stop empty-mongodb@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false unload console-discovery@${INSTANCE}.service
 
 #remove all traces of service files on cluster - needed in case the checked in service file has changed
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false destroy console@${INSTANCE}.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false destroy console@.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false destroy empty-mongodb@${INSTANCE}.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false destroy empty-mongodb@.service
+do_shell_fail fleetctl -strict-host-key-checking=false destroy console@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false destroy console@.service
+do_shell_fail fleetctl -strict-host-key-checking=false destroy empty-mongodb@${INSTANCE}.service
+do_shell_fail fleetctl -strict-host-key-checking=false destroy empty-mongodb@.service
 
 #fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload console@${INSTANCE}.service
 #fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false unload empty-mongodb@${INSTANCE}.service
-do_shell_fail fleetctl -tunnel mink --endpoint http://192.168.1.58:4001 -strict-host-key-checking=false list-units
+do_shell_fail fleetctl -strict-host-key-checking=false list-units
 date_echo "console should be dead now"
 
