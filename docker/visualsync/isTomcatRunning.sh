adab6ed02de9b1c759300c4cb5d9982cf6fc7092
Gary Clayburg <gclaybur@comcast.net>
2015-06-23 09:36:53 -0600

    upgrading spring boot to 1.2.4.RELEASE

--- docker/visualsync/isTomcatRunning.sh
+++ docker/visualsync/isTomcatRunning.sh
@@ -6,10 +6,13 @@ LOG_CONTEXT="$0 -"
 URL=$1
 date_echo "checking for running tomcat at: $URL"
 if [ -x /usr/bin/curl ]; then
-  until [ "`curl --silent --show-error --connect-timeout 1 -I $URL 2> /dev/null | grep 'Coyote'`" != "" ];
+  typeset -i max_seconds=180
+  typeset -i counter=0
+  until [[ "`curl --silent --show-error --connect-timeout 1 -I $URL 2> /dev/null | grep -E 'Coyote|Jetty'`" != "" || ($counter == $max_seconds ) ]];
   do
     date_echo "waiting for tomcat to respond"
     sleep 1
+    counter=$((counter +1))
   done
     date_echo "Tomcat is ready!"
 else
